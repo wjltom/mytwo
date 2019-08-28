8c486564f2d374988a857336ae8f6b4860956ac5
unknown <you@example.com>
2019-08-28 17:40:53 +0800

    第一个

---
+++ MyPrj/js/zj.js
@@ -0,0 +1,5 @@
+﻿//封装组件:
+Vue.component("hulei", {
+    props: ["svalue", "data"],  //特别注意:属性名不能大写
+    template: '<select v-model:value="svalue"><option value="0">请选择</option><option v-for="cls in data" v-bind:value="cls.classId">{{cls.className}}</option></select>'
+});
---
+++ MyPrj2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyPrj2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //Post
+        [HttpPost]
+        public ActionResult Save()
+        {
+            return Json(100);
+        }
+
+    }
+}
